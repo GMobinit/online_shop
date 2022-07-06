99b82c99b32727bd09e0f4e4d6323f7ddb7818c1
GMobinit <vicente@zeevou.com>
2022-07-06 18:23:29 +0430

    Tsk-02 -> entities for shop module implemented. repositories implemented. User controller and User service implemented too.

---
+++ shop/src/main/java/com/shop/product/Product.java
@@ -0,0 +1,20 @@
+package com.shop.product;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@Data
+@Document
+public class Product {
+    @Id
+    private String id;
+    private Category category;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Currency currency;
+}
